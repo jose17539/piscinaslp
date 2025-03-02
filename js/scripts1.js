f109d8f3a8bead09e0faf8b56ea54747a3366af2
jose17539 <jsanchezg165@gmail.com>
2025-03-02 03:03:48 +0100

    Add files via upload

---
+++ js/scripts1.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+   
+      
+       
+            let noticiasHTML = "";
+            {
+                noticiasHTML += `
+                    <article>
+                        
+                    </article>
+                `;
+            };
+            document.getElementById("contenedor-noticias").innerHTML = noticiasHTML;
+        })
+       
